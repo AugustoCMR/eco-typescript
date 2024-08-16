ac28040f969f64a57b8843cd00a1f6a45a344e99
Augusto Ribeiro <augustoribeiro100.g.j@gmai.com>
2024-08-16 19:40:37 -0300

    Criando um middleware para validar os itens passados no detalhe se existem.

--- src/middlewares/abstractMiddleware.ts
+++ src/middlewares/abstractMiddleware.ts
@@ -27,7 +27,8 @@ export function validateIdParam(repository: Repository<any>,errorMessage: string
 
 export function validateIdBody(repository: Repository<any>, bodyName: string, columnNameId: string, errorMessage: string)
 {
-    return async (req: Request, res: Response, next: NextFunction) => {
+    return async (req: Request, res: Response, next: NextFunction) => 
+    {
         try 
         {   
             const body = req.body[bodyName];   
@@ -38,7 +39,8 @@ export function validateIdBody(repository: Repository<any>, bodyName: string, co
 
                 const item = await repository.findOneBy({ codigo: itemId });
 
-                if (!item) {
+                if (!item) 
+                {
                     return res.status(400).json({ message: errorMessage });
                 }
     
@@ -55,4 +57,37 @@ export function validateIdBody(repository: Repository<any>, bodyName: string, co
             return res.status(500).json({ mensagem: `Ocorreu um erro interno do servidor: ${error}` });
         }
     };
+}
+
+export function validateIdDetail(repository: Repository<any>, bodyName: string, columnNameId: string, errorMessage: string)
+{
+    return async (req: Request, res: Response, next: NextFunction) =>
+    {
+        try 
+        {
+            const body = req.body[bodyName];  
+
+            if(Array.isArray(body))
+            {
+                for (const column of body)
+                {
+                    const itemId = parseInt(column[columnNameId]);
+        
+                    const item = await repository.findOneBy({ codigo: itemId });
+        
+                    if(!item)
+                    {
+                        return res.status(400).json({ message: errorMessage });
+                    }
+                }
+            }
+    
+            next();    
+        } 
+        catch (error) 
+        {
+            console.error("Erro ao validar ID:", error);
+            return res.status(500).json({ mensagem: `Ocorreu um erro interno do servidor: ${error}` });
+        }
+    }
 }
