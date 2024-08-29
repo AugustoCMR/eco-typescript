a1e10ae21ac33704061d974c20178cb537878ef2
Augusto Ribeiro <augustoribeiro100.g.j@gmai.com>
2024-08-29 18:52:47 -0300

    Refatorando validação do customer

--- src/validators/customerValidator.ts
+++ src/validators/customerValidator.ts
@@ -4,11 +4,16 @@ export const customerSchema = z.object
 (
     {
         codigo: z.number().optional(),
-        nome: z.string().min(1, "O campo nome é obrigatório"),
+        nome: z.string
+        (
+            {
+                    required_error: "Nome é obrigatório"
+            }
+        ).min(1, "Nome é obrigatório"),
         email: z.string
         (
             {
-                required_error: "O campo e-mail é obrigatório"
+                required_error: "E-mail é obrigatório"
             }
         ).email("O e-mail deve ser válido"),
         senha: z.string
@@ -20,7 +25,7 @@ export const customerSchema = z.object
         ecosaldo: z.number
         (
             {
-                invalid_type_error: "O campo Eco Saldo deve conter apenas números"
+                    invalid_type_error: "O campo Eco Saldo deve conter apenas números"
             }
         ).optional(), 
         cpf: z.string
@@ -32,9 +37,24 @@ export const customerSchema = z.object
         )
         .length(11, "CPF deve conter 11 dígitos")
         .regex(/^\d+$/, "CPF deve conter apenas números"), 
-        pais: z.string().min(1, "O campo país é obrigatório"),
-        estado: z.string().min(1, "O campo estado é obrigatório"),
-        cidade: z.string().min(1, "O campo cidade é obrigatório"),
+        pais: z.string
+        (
+            {
+                    required_error: "País é obrigatório"
+            }
+        ).min(1, "O campo país é obrigatório"),
+        estado: z.string
+        (
+            {
+                    required_error: "Estado é obrigatório"
+            }
+        ).min(1, "O campo estado é obrigatório"),
+        cidade: z.string
+        (
+            {
+                    required_error: "Cidade é obrigatório"
+            }
+        ).min(1, "O campo cidade é obrigatório"),
         cep: z.string
         (
             {
@@ -43,8 +63,23 @@ export const customerSchema = z.object
             }
         )
         .regex(/^\d+$/, "O campo CEP deve conter apenas números"),
-        rua: z.string().min(1, "O campo rua é obrigatório"),
-        bairro: z.string().min(1, "O campo bairro é obrigatório"),
-        numero: z.string().min(1, "O campo número é obrigatório")
+        rua: z.string
+        (
+            {
+                    required_error: "Rua é obrigatório"
+            }
+        ).min(1, "O campo rua é obrigatório"),
+        bairro: z.string
+        (
+            {
+                    required_error: "Bairro é obrigatório"
+            }
+        ).min(1, "O campo bairro é obrigatório"),
+        numero: z.string
+        (
+            {
+                    required_error: "País é obrigatório"
+            }
+        ).min(1, "O campo número é obrigatório")
     }   
 );
