2d0c3844ebbcebf1d4e445253329e422dd6c991f
Augusto Ribeiro <augustoribeiro100.g.j@gmail.com>
2024-09-04 15:14:38 -0300

    Criando testes unitários para o endpoint updateCustomer

--- src/__tests__/services/customerService.test.ts
+++ src/__tests__/services/customerService.test.ts
@@ -3,7 +3,8 @@ import {customerRepository} from "../../repositories/customerRepository";
 import { CodeGenerator } from "../../utils/codeGenerator";
 import { generateHash } from "../../utils/hash";
 import { customerSchema } from "../../validators/customerValidator";
-import {BadRequestError} from "../../helpers/api-erros";
+import {BadRequestError, NotFoundError} from "../../helpers/api-erros";
+import { validateIdParam } from "../../utils/validations";
 
 jest.mock('../../repositories/customerRepository', () => {
     return {
@@ -11,6 +12,7 @@ jest.mock('../../repositories/customerRepository', () => {
             create: jest.fn(),
             save: jest.fn(),
             findOneBy: jest.fn(),
+            update: jest.fn()
         },
     };
 });
@@ -24,6 +26,7 @@ jest.mock('../../data-source', () => {
         },
     };
 });
+jest.mock('../../utils/validations');
 
 describe('CustomerService', () =>
 {
@@ -33,6 +36,7 @@ describe('CustomerService', () =>
     const customerRepositorySaveMock = customerRepository.save as jest.Mock;
     const CodeGeneratorMock = CodeGenerator.prototype.generateCode as jest.Mock;
     const generateHashMock = generateHash as jest.Mock;
+    let validationMock = validateIdParam as jest.Mock;
 
     beforeEach(() => {
         jest.clearAllMocks();
@@ -40,94 +44,13 @@ describe('CustomerService', () =>
         customerService.validateCPFAndEmail = jest.fn();
     });
 
-    it('Register Successful Customer', async () => {
-
-        const mockCustomer: customerSchema = {
-            nome: 'Augusto',
-            email: 'augusto@email.com',
-            senha: '1234',
-            ecosaldo: 1,
-            cpf: '12345678911',
-            pais: 'brasil',
-            estado: 'bahia',
-            cidade: 'salvador',
-            cep: '123',
-            rua: 'aaa',
-            bairro: 'aaaa',
-            numero: 'aaa',
-        }
-
-        const mockCode = 12345;
-        const password = mockCustomer.senha;
-        const mockHashedPassword = 'hashedPassword';
-
-        //Arrange
-        customerRepositoryCreateMock.mockReturnValue(mockCustomer);
-        customerRepositorySaveMock.mockResolvedValue(mockCustomer);
-        CodeGeneratorMock.mockReturnValue(mockCode);
-        generateHashMock.mockReturnValue(mockHashedPassword);
-
-        //Act
-        await customerService.createCustomer(mockCustomer);
-
-        //Assert
-        expect(customerService.validateCPFAndEmail).toHaveBeenCalledTimes(1);
-        expect(CodeGenerator.prototype.generateCode).toHaveBeenCalledTimes(1);
-        expect(generateHash).toHaveBeenCalledTimes(1);
-        expect(customerRepository.create).toHaveBeenCalledTimes(1);
-        expect(customerRepository.save).toHaveBeenCalledTimes(1);
-
-        expect(CodeGenerator.prototype.generateCode).toHaveBeenCalledWith('customer');
-        expect(generateHash).toHaveBeenCalledWith(password);
-        expect(customerRepository.create).toHaveBeenCalledWith({
-            ...mockCustomer,
-            codigo: mockCode,
-            senha: mockHashedPassword,
-        });
-        expect(customerRepository.save).toHaveBeenCalledWith({
-            ...mockCustomer,
-            codigo: mockCode,
-            senha: mockHashedPassword,
-        });
-    });
-
-    it('Register Failed Customer - CPF already exists', async () =>
-    {
-        const mockCustomer: customerSchema =
+    describe('Create Customer', () => {
+        it('Register Successful Customer', async () =>
         {
-            nome: 'Augusto',
-            email: 'aaaa',
-            senha: '1234',
-            ecosaldo: 1,
-            cpf: '12345678911',
-            pais: 'brasil',
-            estado: 'bahia',
-            cidade: 'salvador',
-            cep: '123',
-            rua: 'aaa',
-            bairro: 'aaaa',
-            numero: 'aaa',
-        }
 
-        customerService.validateCPFAndEmail = jest.fn(() => {
-            throw new BadRequestError('O CPF informado já possui cadastro');
-        });
-
-        await expect(customerService.createCustomer(mockCustomer)).rejects.toThrow(BadRequestError);
-
-        expect(customerService.validateCPFAndEmail).toHaveBeenCalledTimes(1);
-        expect(CodeGenerator.prototype.generateCode).not.toHaveBeenCalledTimes(1);
-        expect(generateHash).not.toHaveBeenCalledTimes(1);
-        expect(customerRepository.create).not.toHaveBeenCalledTimes(1);
-        expect(customerRepository.save).not.toHaveBeenCalledTimes(1);
-    });
-
-    it('Register Failed Customer - E-mail already exists', async () =>
-    {
-        const newCustomer: customerSchema =
-        {
+            const mockCustomer: customerSchema = {
                 nome: 'Augusto',
-                email: 'augusto@example.com',
+                email: 'augusto@email.com',
                 senha: '1234',
                 ecosaldo: 1,
                 cpf: '12345678911',
@@ -138,26 +61,175 @@ describe('CustomerService', () =>
                 rua: 'aaa',
                 bairro: 'aaaa',
                 numero: 'aaa',
-        }
+            }
 
-        const validateCPFAndEmailMock = jest.fn(() => {
-            throw new BadRequestError('O E-mail informado já possui cadastro');
+            const mockCode = 12345;
+            const password = mockCustomer.senha;
+            const mockHashedPassword = 'hashedPassword';
+
+            //Arrange
+            customerRepositoryCreateMock.mockReturnValue(mockCustomer);
+            customerRepositorySaveMock.mockResolvedValue(mockCustomer);
+            CodeGeneratorMock.mockReturnValue(mockCode);
+            generateHashMock.mockReturnValue(mockHashedPassword);
+
+            //Act
+            await customerService.createCustomer(mockCustomer);
+
+            //Assert
+            expect(customerService.validateCPFAndEmail).toHaveBeenCalledTimes(1);
+            expect(CodeGenerator.prototype.generateCode).toHaveBeenCalledTimes(1);
+            expect(generateHash).toHaveBeenCalledTimes(1);
+            expect(customerRepository.create).toHaveBeenCalledTimes(1);
+            expect(customerRepository.save).toHaveBeenCalledTimes(1);
+
+            expect(CodeGenerator.prototype.generateCode).toHaveBeenCalledWith('customer');
+            expect(generateHash).toHaveBeenCalledWith(password);
+            expect(customerRepository.create).toHaveBeenCalledWith({
+                ...mockCustomer,
+                codigo: mockCode,
+                senha: mockHashedPassword,
+            });
+            expect(customerRepository.save).toHaveBeenCalledWith({
+                ...mockCustomer,
+                codigo: mockCode,
+                senha: mockHashedPassword,
+            });
         });
 
-        customerService.validateCPFAndEmail = validateCPFAndEmailMock;
+        it('Register Failed Customer - CPF already exists', async () =>
+        {
+            const mockCustomer: customerSchema =
+                {
+                    nome: 'Augusto',
+                    email: 'aaaa',
+                    senha: '1234',
+                    ecosaldo: 1,
+                    cpf: '12345678911',
+                    pais: 'brasil',
+                    estado: 'bahia',
+                    cidade: 'salvador',
+                    cep: '123',
+                    rua: 'aaa',
+                    bairro: 'aaaa',
+                    numero: 'aaa',
+                }
 
-        await expect(customerService.createCustomer(newCustomer)).rejects.toThrow(BadRequestError);
+            customerService.validateCPFAndEmail = jest.fn(() => {
+                throw new BadRequestError('O CPF informado já possui cadastro');
+            });
 
-        expect(validateCPFAndEmailMock).toHaveBeenCalledTimes(1);
-        expect(CodeGenerator.prototype.generateCode).not.toHaveBeenCalledTimes(1);
-        expect(generateHash).not.toHaveBeenCalledTimes(1);
-        expect(customerRepository.create).not.toHaveBeenCalledTimes(1);
-        expect(customerRepository.save).not.toHaveBeenCalledTimes(1);
+            await expect(customerService.createCustomer(mockCustomer)).rejects.toThrow(BadRequestError);
+
+            expect(customerService.validateCPFAndEmail).toHaveBeenCalledTimes(1);
+            expect(CodeGenerator.prototype.generateCode).not.toHaveBeenCalledTimes(1);
+            expect(generateHash).not.toHaveBeenCalledTimes(1);
+            expect(customerRepository.create).not.toHaveBeenCalledTimes(1);
+            expect(customerRepository.save).not.toHaveBeenCalledTimes(1);
+        });
+
+        it('Register Failed Customer - E-mail already exists', async () =>
+        {
+            const newCustomer: customerSchema =
+                {
+                    nome: 'Augusto',
+                    email: 'augusto@example.com',
+                    senha: '1234',
+                    ecosaldo: 1,
+                    cpf: '12345678911',
+                    pais: 'brasil',
+                    estado: 'bahia',
+                    cidade: 'salvador',
+                    cep: '123',
+                    rua: 'aaa',
+                    bairro: 'aaaa',
+                    numero: 'aaa',
+                }
+
+            const validateCPFAndEmailMock = jest.fn(() => {
+                throw new BadRequestError('O E-mail informado já possui cadastro');
+            });
+
+            customerService.validateCPFAndEmail = validateCPFAndEmailMock;
+
+            await expect(customerService.createCustomer(newCustomer)).rejects.toThrow(BadRequestError);
+
+            expect(validateCPFAndEmailMock).toHaveBeenCalledTimes(1);
+            expect(CodeGenerator.prototype.generateCode).not.toHaveBeenCalledTimes(1);
+            expect(generateHash).not.toHaveBeenCalledTimes(1);
+            expect(customerRepository.create).not.toHaveBeenCalledTimes(1);
+            expect(customerRepository.save).not.toHaveBeenCalledTimes(1);
+        });
     });
+
+    describe('Update Customer', () =>
+    {
+        it('Updated Sucess Customer', async () =>
+        {
+            const code = 1;
+            const mockCustomer: customerSchema =
+            {
+                nome: 'Augusto',
+                email: 'augusto@email.com',
+                senha: '1234',
+                ecosaldo: 1,
+                cpf: '12345678911',
+                pais: 'brasil',
+                estado: 'bahia',
+                cidade: 'salvador',
+                cep: '123',
+                rua: 'aaa',
+                bairro: 'aaaa',
+                numero: 'aaa',
+            }
+
+            validationMock.mockReturnValue(mockCustomer);
+
+            await customerService.updateCustomer(code, mockCustomer);
+
+            expect(validationMock).toHaveBeenCalledTimes(1);
+
+            expect(validationMock).toHaveBeenCalledWith(customerRepository, "usuário", code);
+            expect(customerRepository.update).toHaveBeenCalledWith(
+                {
+                    codigo: code
+                },
+                {
+                    ...mockCustomer
+                }
+            );
+        });
+
+        it('Updated Failed Customer - Customer Not Found', async () =>
+        {
+            const code = 2;
+            const mockCustomer: customerSchema =
+            {
+                    nome: 'Augusto',
+                    email: 'augusto@email.com',
+                    senha: '1234',
+                    ecosaldo: 1,
+                    cpf: '12345678911',
+                    pais: 'brasil',
+                    estado: 'bahia',
+                    cidade: 'salvador',
+                    cep: '123',
+                    rua: 'aaa',
+                    bairro: 'aaaa',
+                    numero: 'aaa',
+            }
+
+            validationMock = jest.fn(() => {
+                throw new NotFoundError("O ID do usuário não foi encontrado");
+            })
+
+            await expect(customerService.updateCustomer(code, mockCustomer)).rejects.toThrow(NotFoundError);
+
+            expect(validationMock).toHaveBeenCalledTimes(1);
+            expect(customerRepository.update).not.toHaveBeenCalledTimes(1);
+        })
+
+    })
+
+
 });
-
-
-
-
-
-
